3938293c524c945fd76784ebc4bd68027a30d365
Peter Kofler <peter.kofler@code-cop.org>
2019-05-19 21:46:42 +0200

    Go buy.

--- typescript/beer-song.ts
+++ typescript/beer-song.ts
@@ -1,21 +1,28 @@
 export default class Beer {
 
     static verse(n: number): string {
-        return `${Beer.bottles(n)} of beer on the wall, ${Beer.bottles(n)} of beer.
-Take ${Beer.it(n)} down and pass it around, ${Beer.bottles(n - 1)} of beer on the wall.
+        const wall = `${Beer.bottles(n, true)} of beer on the wall, ${Beer.bottles(n)} of beer.
 `
+        const take = `Take ${Beer.it(n)} down and pass it around, ${Beer.bottles(n - 1)} of beer on the wall.
+`
+        const buy = `Go to the store and buy some more, 99 bottles of beer on the wall.
+`
+        return wall + (n > 0 ? take : buy)
     }
 
     static it(n: number): string {
         return n > 1 ? 'one' : 'it'
     }
 
-    private static bottles(n: number): string {
+    private static bottles(n: number, upper: boolean = false): string {
         if (n > 1) {
             return `${n} bottles`
         }
         else if (n === 1) {
             return `${n} bottle`
+        }
+        else if (upper) {
+            return `No more bottles`
         }
         else {
             return `no more bottles`
