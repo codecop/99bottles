28e739b0b4266a3022d2af18fdb6c2f7cc1e64d6
Peter Kofler <peter.kofler@code-cop.org>
2019-05-19 22:49:15 +0200

    last_generic_verse.

--- ruby/beer_song.rb
+++ ruby/beer_song.rb
@@ -1,7 +1,20 @@
 class BeerSong
 
   def verse(number)
-    "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+    Verse.new(number).to_s
+  end
+
+end
+
+class Verse
+
+  def initialize(number)
+    @number = number
+  end
+
+  def to_s
+    "#{@number} bottles of beer on the wall, #{@number} bottles of beer." + "\n"+
+    "Take one down and pass it around, #{@number - 1} bottles of beer on the wall." + "\n"
   end
 
 end
--- ruby/beer_song_test.rb
+++ ruby/beer_song_test.rb
@@ -15,7 +15,6 @@ Take one down and pass it around, 98 bottles of beer on the wall.
   end
 
   def test_last_generic_verse
-    skip
     expected = <<-TEXT
 3 bottles of beer on the wall, 3 bottles of beer.
 Take one down and pass it around, 2 bottles of beer on the wall.
