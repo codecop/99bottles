4a779cf9e6082703fcd1fce1d7b6c99c04c40370
Peter Kofler <peter.kofler@code-cop.org>
2019-05-19 22:03:52 +0200

    Cleanup "verse".

--- typescript/beer-song.ts
+++ typescript/beer-song.ts
@@ -1,32 +1,54 @@
 export default class Beer {
 
-    static verse(n: number): string {
-        const wall = `${Beer.bottles(n, true)} of beer on the wall, ${Beer.bottles(n)} of beer.
-`
-        const take = `Take ${Beer.it(n)} down and pass it around, ${Beer.bottles(n - 1)} of beer on the wall.
-`
-        const buy = `Go to the store and buy some more, 99 bottles of beer on the wall.
-`
-        return wall + (n > 0 ? take : buy)
+    public static verse(numberOfBottles: number): string {
+        return Beer.firstLine(numberOfBottles) +
+            Beer.secondLine(numberOfBottles)
     }
 
-    static it(n: number): string {
-        return n > 1 ? 'one' : 'it'
+    private static firstLine(numberOfBottles: number): string {
+        const bottles = Beer.bottles(numberOfBottles)
+        return Beer.capitalizeSentence(`${bottles} of beer on the wall, ${bottles} of beer.
+`)
     }
 
-    private static bottles(n: number, upper: boolean = false): string {
-        if (n > 1) {
-            return `${n} bottles`
+    private static bottles(numberOfBottles: number): string {
+        if (numberOfBottles > 1) {
+            return `${numberOfBottles} bottles`
         }
-        else if (n === 1) {
-            return `${n} bottle`
-        }
-        else if (upper) {
-            return `No more bottles`
+        else if (numberOfBottles === 1) {
+            return `${numberOfBottles} bottle`
         }
         else {
             return `no more bottles`
         }
+    }
+
+    private static capitalizeSentence(sentence: string): string {
+        return sentence.substr(0, 1).toUpperCase() + sentence.substr(1)
+    }
+
+    private static secondLine(numberOfBottles: number): string {
+        if (numberOfBottles > 0) {
+            return Beer.takeOneDown(numberOfBottles)
+        } else {
+            return Beer.goToTheStore()
+        }
+    }
+
+    private static takeOneDown(numberOfBottles: number) {
+        const it = Beer.it(numberOfBottles)
+        const remainingBottles = Beer.bottles(numberOfBottles - 1)
+        return `Take ${it} down and pass it around, ${remainingBottles} of beer on the wall.
+`
+    }
+
+    private static it(numberOfBottles: number): string {
+        return numberOfBottles > 1 ? 'one' : 'it'
+    }
+
+    private static goToTheStore() {
+        return `Go to the store and buy some more, 99 bottles of beer on the wall.
+`
     }
 
     static sing(start: number = 99, end: number = 0): string {
