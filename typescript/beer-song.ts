14816fc4d0c3fd12c816122756dcf4f2b659ff5b
Peter Kofler <peter.kofler@code-cop.org>
2019-05-19 21:36:24 +0200

    Fix whitespace to pass first tests.

--- typescript/beer-song.ts
+++ typescript/beer-song.ts
@@ -2,8 +2,8 @@ export default class Beer {
 
     static verse(n: number): string {
         return `${n} bottles of beer on the wall, ${n} bottles of beer.
-    Take one down and pass it around, ${n - 1} bottles of beer on the wall.
-    `
+Take one down and pass it around, ${n - 1} bottles of beer on the wall.
+`
     }
 
     static sing(start: number = 99, end: number = 0): string {
