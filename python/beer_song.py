88ab648d153ee0ff9bec3d8bcee9370b72b91147
Peter Kofler <peter.kofler@code-cop.org>
2019-06-01 00:00:54 +0200

    Fakes first test.

--- python/beer_song.py
+++ python/beer_song.py
@@ -1,2 +1,3 @@
 def recite(start, take=1):
-    pass
+    return ["99 bottles of beer on the wall, 99 bottles of beer.",
+            "Take one down and pass it around, 98 bottles of beer on the wall."]
