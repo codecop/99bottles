3984fbf23fc9c624717d1024d5df5844ae37fa41
Peter Kofler <peter.kofler@code-cop.org>
2019-06-01 00:15:04 +0200

    99 and 98.

--- python/beer_song.py
+++ python/beer_song.py
@@ -1,13 +1,14 @@
 def recite_gen(start):
     yield "99 bottles of beer on the wall, 99 bottles of beer."
     yield "Take one down and pass it around, 98 bottles of beer on the wall."
+    yield ""
+    yield "98 bottles of beer on the wall, 98 bottles of beer."
+    yield "Take one down and pass it around, 97 bottles of beer on the wall."
+    yield ""
 
 
 # adapt to required API
 def recite(start, take=1):
-    if take == 1:
-        take_lines = 2
-    else:
-        take_lines = take * 3
+    take_lines = take * 3 - 1
 
     return [line for line in recite_gen(start)][0:take_lines]
