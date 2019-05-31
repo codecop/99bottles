c5966986e28bd32e94ae503e2ef1afd0cb6cd3ce
Peter Kofler <peter.kofler@code-cop.org>
2019-06-01 00:12:37 +0200

    Adapts generator to required API.

--- python/beer_song.py
+++ python/beer_song.py
@@ -1,3 +1,13 @@
+def recite_gen(start):
+    yield "99 bottles of beer on the wall, 99 bottles of beer."
+    yield "Take one down and pass it around, 98 bottles of beer on the wall."
+
+
+# adapt to required API
 def recite(start, take=1):
-    return ["99 bottles of beer on the wall, 99 bottles of beer.",
-            "Take one down and pass it around, 98 bottles of beer on the wall."]
+    if take == 1:
+        take_lines = 2
+    else:
+        take_lines = take * 3
+
+    return [line for line in recite_gen(start)][0:take_lines]
