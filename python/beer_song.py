9900a896746b1b988fe6223a8bed5669d401fa0b
Peter Kofler <peter.kofler@code-cop.org>
2019-06-01 00:26:34 +0200

    All green copying over the expected verses.

--- python/beer_song.py
+++ python/beer_song.py
@@ -7,6 +7,15 @@ def recite_gen(start):
         yield "Take one down and pass it around, {number} bottles of beer on the wall.".format(number=bottles)
         yield ""
 
+    yield "2 bottles of beer on the wall, 2 bottles of beer."
+    yield "Take one down and pass it around, 1 bottle of beer on the wall."
+    yield ""
+    yield "1 bottle of beer on the wall, 1 bottle of beer."
+    yield "Take it down and pass it around, no more bottles of beer on the wall."
+    yield ""
+    yield "No more bottles of beer on the wall, no more bottles of beer."
+    yield "Go to the store and buy some more, 99 bottles of beer on the wall."
+
 
 # adapt to required API
 def recite(start, take=1):
